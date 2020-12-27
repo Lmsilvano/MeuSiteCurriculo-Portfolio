9b4f9a2d6433ca708701e73f0fc5509cbc3b373e
Lmsilvano <75850731+Lmsilvano@users.noreply.github.com>
2020-12-27 17:57:27 -0300

    atualizações feitas
    adição de interatividade no menu com js, finalização de home, começo do sobre mim, adição de crédito

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,9 @@
+
 // Exibir Menu
 
 const showMenu = (toggleId, navId) =>{
-    const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId),
+    const toggle = document.getElementById(toggleId)
+    nav = document.getElementById(navId)
    
     if(toggle && nav){
         toggle.addEventListener('click', ()=>{
@@ -11,3 +12,38 @@ const showMenu = (toggleId, navId) =>{
     }
 }
 showMenu('nav-toggle','nav-menu')
+
+// Remover o menu no Mobile
+
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+// Scroll sec e ativação de link
+
+const sections = document.querySelectorAll('section[id]')
+
+window.addEventListener('scroll', scrollActive)
+
+function scrollActive(){
+    const scrollY = window.pageYOFFset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active')
+        } else{
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active')
+
+        }
+    })
+
+
+}
